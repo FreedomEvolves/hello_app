036fdb3d9bd91eab5ab87d0bde239b7bd49462be
freedomevolves <samskinnerphd@gmail.com>
2014-10-07 10:37:27 -0400

    add User and Micropost models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#adios'
+  resources :microposts
+  resources :users
+  root 'users#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
